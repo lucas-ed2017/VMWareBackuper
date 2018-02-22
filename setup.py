59bad4740c7ffc86d028f92c5d6d7d01e0c750a9
lucas-ed2017 <35579546+lucas-ed2017@users.noreply.github.com>
2018-02-22 18:05:48 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'vmwarebackuper',
+    version = '1.0.0',
+    py_modules = ['vmwarebackuper'],
+    author = 'simon',
+    author_email = 'dddsimonddd@hotmail.com',
+    url = '',
+    description = 'A module for do backup of a vmware esxi server and save in a ftp server',
+    )
