4f0327da181cbf20da1b3ca0b66b23945fc0a932
Marco Antonio Silva <marcocspc@MacBook-Air-de-Marco.local>
2018-03-18 14:18:32 -0300

    Progress bar now available for ftpserver.sendfile

--- vmwarebackuper/ftpserver.py
+++ vmwarebackuper/ftpserver.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 import ftplib #biblioteca FTP
 import progressbar
-from os import system		 #comando system permite dar comandos pelo terminal
+from os import system		 #biblioteca system permite dar comandos pelo terminal
 from os import stat
 
-class ftpserver(object):	#inicio da classe
+class ftpserver:	#inicio da classe
 	
-	def __init__(self, address, user, password, baseDir):	#construtor
+	def __init__(self, address, user, password, baseDir, progressbar = True):	#construtor
 		self.address = address	#metodos
 		self.user = user
 		self.password = password
@@ -15,45 +15,49 @@ class ftpserver(object):	#inicio da classe
 		self.ftp = ftplib.FTP(self.address)
 		self.ftp.login(user=self.user, passwd=self.password)
 		self.ftp.cwd(self.baseDir)
-		#self.connect()
+		self.connect()
+		self.progressbar = progressbar
 
 	def sendfile(self, file):	#compactar e enviar a maquina compactada
                 try:
-                        #self.connect()
-                        total = stat(file).st_size
-                        bar = progressbar.progressbar(0, total)
-                        def create_callback():
-                                def cb(data):
-                                        bar.update_progress(len(data)/total)
-
-                                return cb
-                        callback = create_callback()
-                        self.ftp.storbinary("STOR " + file, open(file, 'rb'), callback=create_callback())
+                        self.connect()
+                        if self.progressbar: #se for necessario utilizar a progressbar, crie uma e envie o arquivo
+                                total = stat(file).st_size
+                                bar = progressbar.progressbar(0, total)
+                                cb = self.__create_callback__(bar, total)
+                                self.ftp.storbinary("STOR " + file, open(file, 'rb'), callback=cb)
+                        else:   #se nao, apenas envie o arquivo silenciosamente
+                                self.ftp.storbinary("STOR " + file, open(file, 'rb'))
                         self.end()
                 except ftplib.all_errors as e:
                         print(e)
                         self.end()
-                        #self.connected = False
+                        self.connected = False
                         raise
 
-	#def downloadfile(self, file):
-        #        self.connect()
-#		self.ftp.retrbinary('RETR ' + file, open(file, 'wb').write())	#baixar um sistema requisitado
-#		self.end()
+	def downloadfile(self, file):
+                self.connect()
+		self.ftp.retrbinary('RETR ' + file, open(file, 'wb').write())	#baixar um sistema requisitado
+		self.end()
 
-	#def connect(self):
-        #        try:	#tentar estabelecer conexão FTP
-        #                self.ftp = ftplib.FTP(self.address, timeout = 100) #conexão com o servidor
-        #                self.ftp.login(user=self.user, passwd=self.password)
-        #                self.ftp.set_pasv(False) #entra no modo ativo do FTP, necessário para alguns ambientes, estudar se o modo ativo funciona em todos os ambientes, no IFRN foi necessário
-        #                self.ftp.cwd(self.baseDir)
-        #                self.connected = True
-        #                print('FTP Connection successfull!')
-#		except ftplib.all_errors as e:	#conexão falhou
-#			self.connected = False
-#			print(e)
-#			raise
+	def connect(self):
+                try:	#tentar estabelecer conexão FTP
+                        self.ftp = ftplib.FTP(self.address, timeout = 100) #conexão com o servidor
+                        self.ftp.login(user=self.user, passwd=self.password)
+                        self.ftp.set_pasv(False) #entra no modo ativo do FTP, necessário para alguns ambientes, estudar se o modo ativo funciona em todos os ambientes, no IFRN foi necessário
+                        self.ftp.cwd(self.baseDir)
+                        self.connected = True
+                        print('FTP Connection successfull!')
+		except ftplib.all_errors as e:	#conexão falhou
+			self.connected = False
+			print(e)
+			raise
 
 	def end(self):
-                #self.connected = False
+                self.connected = False
                 self.ftp.quit()
+
+        def __create_callback__(self, progressbar, total): #os underlines no nome da funcao tornam ela 'private', o que dificulta a utilizacao em outras classes
+                def cb(data):
+                        progressbar.update_progress(len(data))
+                return cb
--- vmwarebackuper/progressbar.py
+++ vmwarebackuper/progressbar.py
@@ -1,26 +1,42 @@
 class progressbar:
 
-    def __init__(self, progress, total):
-        self.progress = progress
-        self.total = total
+    def __init__(self, progress, total, barlength = None):
+        self.progress = float(progress)
+        self.total = float(total)
+
+        try:
+            import console
+            if barlength == None: #verifica se o tamanho da barlength eh o padrao
+                (self.barlength, throwaway) = console.getTerminalSize() #se eh o tamanho padrao tenta pegar o tamanho do console e usa-lo
+        except ImportError: #se a importacao de console der errado, usar 50 como tamanho padrao da barlength
+            if barlength == None: #verifica se o tamanho da barlength eh o padrao
+                self.barlength = 50
+            
 
     def update_progress(self, progress):
-        barLength = 50
+        barLength = self.barlength
         status = ""
         self.progress += progress
+
+        progress = self.progress/self.total
+        
         if isinstance(progress, int):
-            self.progress = float(progress)
+            progress = float(progress)
         elif not isinstance(progress, float):
-            self.progress = 0
-            status = "error: progress var must be float\r\n"
+            progress = 0
+            status = "error: progress var must be float\r"
+
+        
 
         if progress < 0:
-            self.progress = 0
+            progress = 0
             status = "Halt...\r\n"
         elif progress >= 1:
-            self.progress = 1
+            progress = 1
             status = "Done...\r\n"
 
-        block = int(round(barLength*self.progress))
-        text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength - block), round(self.progress*100, 2), status)
-        print('\r' + (text), end="")
+        block = int(round(barLength*progress))
+        text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength - block), round(progress*100, 2), status)
+        print('\r' + (text), end="") #este eh o modo de printar no python3
+        #este eh o modo de printar no python2
+        #print '\r' + (text),
